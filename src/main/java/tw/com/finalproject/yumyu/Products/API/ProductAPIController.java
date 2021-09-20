050889722642a820c7b045e6dde9b4d76166c0f4
joeyaung <39510619+joeyaung@users.noreply.github.com>
2021-09-20 22:39:17 +0800

    Update ProductAPIController.java

--- src/main/java/tw/com/finalproject/yumyu/Products/API/ProductAPIController.java
+++ src/main/java/tw/com/finalproject/yumyu/Products/API/ProductAPIController.java
@@ -6,6 +6,7 @@ import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -142,11 +143,11 @@ public class ProductAPIController {
 		Object newTags = data.get("tags");
 		Object newImgPath = data.get("imgPath");
 
-		List<String> list = new ArrayList<>();
+		List<String> list = new ArrayList<String>();
 		if (newTags.getClass().isArray()) {
 			list = Arrays.asList((String[]) newTags);
 		} else if (newTags instanceof Collection) {
-			list = new ArrayList<>((Collection<String>) newTags);
+			list = new ArrayList<String>((Collection<String>) newTags);
 		}
 
 		Product product = productService.findById(Long.valueOf(productId));
